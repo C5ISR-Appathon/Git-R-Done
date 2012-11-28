8ef550e7308ae68c8348bf75f3bdddd58803369e
Christopher Temples <ctemples@gmail.com>
2012-11-28 12:58:28 -0500

    only needs to get weight

--- UXO/src/com/team9/uxo/MainActivity.java
+++ UXO/src/com/team9/uxo/MainActivity.java
@@ -3,12 +3,18 @@ package com.team9.uxo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private Spinner explosiveList;
 	private TextView weightText;
+	private int weight;
+	private Spinner calculationType;
+	private String blastType;
+	private TextView radius;
+	
 	
 	
 
@@ -18,6 +24,9 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		explosiveList = (Spinner) findViewById(R.id.explosive_list);
 		weightText = (TextView) findViewById(R.id.explosive_weight);
+		calculationType = (Spinner)findViewById(R.id.calc_method);
+		radius = (TextView) findViewById(R.id.blast_radius);
+		
 	}
 
 	@Override
@@ -26,5 +35,63 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	public void onWeightClick(View v){
+		System.out.println("Weight: " + weightText.getText());
+		System.out.println("Resource: " + getString(R.string.weight));
+		if(weightText.getText().equals(getString(R.string.weight))){
+			System.out.println("match");
+			weightText.setText("test");
+		} else {
+			//String temp = (String) weightText.getText();
+			//weight = Integer.getInteger(temp);
+		}
+	}
+	
+	public void onExplosiveType(View v){
+		
+	}
+	
+	public void onCalcMethodClicked(View v){
+		blastType = (String) calculationType.getSelectedItem();
+	}
+	
+	public double calculateBlastRadius(){
+		double x =0;
+		 if(blastType.equals("Blast")){
+			 x = calcTypeBlast(convertToTNT(weight));
+		 } else if(blastType.equals("Frag")) {
+			 x = calcTypeFrag(convertToTNT(weight));
+		 }
+		 radius.setText(x+"");
+		 return x;
+	}
+	
+	private double convertToTNT(int weight) {
+		String explosiveType = (String) explosiveList.getSelectedItem();
+		if(explosiveType.equals("C4")){
+			return  (weight *1.2);
+		}else if(explosiveType.equals("Composition B")){
+			return weight * 1.3;
+		} else if(explosiveType.equals("Dynamite")) {
+			return weight * 0.9;
+		} else if(explosiveType.equals("Amonimum Nitrate")){
+			return weight * 0.82;
+		} else {
+			return (double) weight;
+		}
+	}
+	
+	private double calcTypeBlast(double x){
+		
+		return x*2;
+	}
+	private double calcTypeFrag(double x) {
+		return x*3;
+	}
+	
+	
+	
+	
 
 }
