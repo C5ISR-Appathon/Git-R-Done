239d4c3ebdb0bfdf6d6de34d02a430c47c418594
Christopher Temples <ctemples@gmail.com>
2012-11-28 19:37:16 -0500

    working with sat view

--- UXO/src/com/team9/uxo/MapsActivity.java
+++ UXO/src/com/team9/uxo/MapsActivity.java
@@ -2,9 +2,14 @@ package com.team9.uxo;
 
 import java.util.List;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -14,6 +19,10 @@ import com.google.android.maps.OverlayItem;
 
 public class MapsActivity extends MapActivity {
 	MapView mapView;
+	private LocationManager locationManager;
+	private String provider;
+	 private TextView latituteField;
+	  private TextView longitudeField;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,8 @@ public class MapsActivity extends MapActivity {
 		setContentView(R.layout.activity_maps);
 		MapView mapView = (MapView) findViewById(R.id.mapview);
 	    mapView.setBuiltInZoomControls(true);
+	    mapView.setSatellite(true);
+	    
 	    
 	    List<Overlay> mapOverlays = mapView.getOverlays();
 	    Drawable drawable = this.getResources().getDrawable(R.drawable.red_cross);
@@ -37,8 +48,32 @@ public class MapsActivity extends MapActivity {
 	    {
 	    	itemizedoverlay.rad = (int) extras.getDouble("radius");
 	    }
-	}
+	    
+//	 // Get the location manager
+//	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//	    // Define the criteria how to select the locatioin provider -> use
+//	    // default
+//	    Criteria criteria = new Criteria();
+//	    provider = locationManager.getBestProvider(criteria, false);
+//	    Location location = locationManager.getLastKnownLocation(provider);
+//
+//	    // Initialize the location fields
+//	    if (location != null) {
+//	      System.out.println("Provider " + provider + " has been selected.");
+//	      onLocationChanged(location);
+//	    } else {
+//	      latituteField.setText("Location not available");
+//	      longitudeField.setText("Location not available");
+//	    }
+	  }
+	    
 
+	public void onLocationChanged(Location location) {
+	    int lat = (int) (location.getLatitude());
+	    int lng = (int) (location.getLongitude());
+	    latituteField.setText(String.valueOf(lat));
+	    longitudeField.setText(String.valueOf(lng));
+  }
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
